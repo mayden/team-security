e8fb3381417916fc46713a83061eb0cb6c06499d
Alex <alex@alnet.co.il>
2017-05-15 18:49:56 +0300

    finish of successfull connection to the server via the client

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ router.post('/login', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.send('POST WORKS');
+
+  res.send('Successfully connected to the server.');
 });
 
 /* login */
