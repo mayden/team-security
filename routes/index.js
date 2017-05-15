65b42e3910f2d4ed708529e8ac799ef7f7c8046f
Alex <alex@alnet.co.il>
2017-05-15 17:51:25 +0300

    trying to upload the bin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+      { title: 'Express' });
 });
+
+/* login */
+router.post('/login', function(req, res, next) {
+  res.send('checking something');
+});
+
 
 module.exports = router;
