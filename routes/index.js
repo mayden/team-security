d04132568ff22dcc8a09c9d78b23aa17226914e7
Bella <bella.gandin@gmail.com>
2017-05-16 17:36:19 +0300

    get the usermane and the password

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,8 @@ router.post('/login', function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
 
   res.send('Successfully connected  to the server.');
-  console.log(req.body);
+  console.log(req.body.username);
+    console.log(req.body.password);
 });
 
 /* login */
