53ad5578fd23bc6a68ebc1fe86c4a211a91db767
Bella <bella.gandin@gmail.com>
2017-05-16 21:30:13 +0300

    get the usermane and the password

--- client-side/app/popup.js
+++ client-side/app/popup.js
@@ -26,8 +26,8 @@ var securityForm = {
         data.append('password', document.getElementById("userpass").value );
         window.alert( document.getElementById("username").value );
         window.alert( document.getElementById("userpass").value );
-        //http.send(data);
-        http.send("&"+document.getElementById("username").value+"&"+document.getElementById("userpass").value);
+        http.send(data);
+       // http.send({"username ": document.getElementById("username").value ,{ "pass": document.getElementById("userpass").value);
     }
 }
 
