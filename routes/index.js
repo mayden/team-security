a8236c0267ae88f24040ea1cb365abcf4c18aae7
Bella <bella.gandin@gmail.com>
2017-05-16 19:37:31 +0300

    get the usermane and the password

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
+
 
 
 /* GET home page. */
@@ -10,23 +12,25 @@ router.get('/', function(req, res, next) {
 
 /* login */
 router.post('/login', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
 
-  res.send('Successfully connected  to the server.');
-  console.log(req.body);
-  console.log(req.body.data);
-  console.log(res.statusCode);
-  console.log(req.body.data.get(username));
-
+    res.send('Successfully connected  to the server.');
+    console.log(req.body);
+    console.log(res.statusCode);
+     MongoClient.connect("mongodb://manager:1234@ds139801.mlab.com:39801/heroku_5277wf1z", function(err, db) {
+       if (!err) {
+           console.log("We are connected");
+        }
+      });
 });
 
-/* login */
-router.get('/login', function(req, res, next) {
-  res.send('LOGIN GET EXAMPLE');
-  window.alert(req);
+    /* login */
+    router.get('/login', function (req, res, next) {
+        res.send('LOGIN GET EXAMPLE');
+        window.alert(req);
 
-});
+    });
 
-module.exports = router;
+    module.exports = router;
