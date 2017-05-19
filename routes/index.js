54b718d8a2b797ef834907cf20e8249785693ea9
Bella <bella.gandin@gmail.com>
2017-05-19 10:29:35 +0300

    get the usermane and the password

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,8 @@ router.post('/login', function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
 
     res.send('Successfully connected  to the server.');
-    console.log(req.body.usename);
+    console.log(req.body.data);
+    console.log(req.body.data);
     console.log(res.statusCode);
      MongoClient.connect("mongodb://manager:1234@ds139801.mlab.com:39801/heroku_5277wf1z", function(err, db) {
        if (!err) {
