ecb6930e7017aff87cbb8b88d9a0e9ac1f6a3e85
Bella <bella.gandin@gmail.com>
2017-05-19 12:05:09 +0300

    trying to send message to the server

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 // create our app
-var router = express.Router();
-
+//var router = express.Router();
+var app = express();
 
 // instruct the app to use the `bodyParser()` middleware for all routes
-router.use(bodyParser());
+app.use(bodyParser());
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index',
       { title: 'Express' });
 });
 
 /* login */
-router.post('/login', function(req, res, next) {
+app.post('/login', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
@@ -23,6 +23,7 @@ router.post('/login', function(req, res, next) {
     res.send('Successfully connected  to the server.');
     console.log(req.body);
     console.log(req.body.username);
+    console.log(req.body[username]);
     console.log(req.username);
     console.log(res.statusCode);
      MongoClient.connect("mongodb://manager:1234@ds139801.mlab.com:39801/heroku_5277wf1z", function(err, db) {
