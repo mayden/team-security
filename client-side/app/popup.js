bf7345bf55818488cddcbee99ea434728b433369
Alex <alex@alnet.co.il>
2017-05-22 16:31:23 +0300

    fixing the server recieving username and password

--- client-side/app/popup.js
+++ client-side/app/popup.js
@@ -1,11 +1,12 @@
 /*
-class of securityForm, holds all the functions that needed to make the connection.
+ class of securityForm, holds all the functions that needed to make the connection.
  */
 var securityForm = {
     // hold the url of the server. Is where we are going to send the credentials.
     server_url : "https://project-security.herokuapp.com/login",
-   // server_url : "http://localhost:3000/login",
-    send: function(username, userpass) {
+    //server_url : "http://localhost:3000/login",
+
+    send: function(username, password) {
 
         var http = new XMLHttpRequest();
         http.open("POST", this.server_url, true);
@@ -21,23 +22,19 @@ var securityForm = {
             }
         }
 
-        var data = new FormData();
-        data.append('username',  username);
-        data.append('password',  userpass );
-        window.alert(data);
-        http.send(data);
-       //http.send(username+"&"+userpass);
+        // send the correct username and password to the server
+        http.send("username=" + username + "&password=" + password);
     }
 }
 
 /*
-* On submitting the form, the username and the password credentials are sent to the server.
-*/
+ * On submitting the form, the username and the password credentials are sent to the server.
+ */
 $('#passForm').on('submit', function(e) {
     e.preventDefault();
 
     var username = $('#username').val();
     var password = $('#userpass').val();
 
-    securityForm.send(username,password);
+    securityForm.send(username, password);
 });
