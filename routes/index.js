29991effdebadddc5e45c49eda672a0e67d3b35b
Bella <bella.gandin@gmail.com>
2017-06-17 14:21:07 +0300

    check if the user is already have a uses in server

--- routes/index.js
+++ routes/index.js
@@ -37,47 +37,15 @@ router.post('/login', function(req, res, next) {
             users.insert(userObject, function (err, doc) {
                 if (err) {
                     res.send("There was a problem adding the information to the database.");
-
                 }
 
             });
         }
-
         res.send('Successfully register to our DB.');
         console.log('Successfully register to our DB.');
         user_id = result._id;
-
+        console.log(user_id);
     });
-
-
-
-}, function (req, res, next) {
-    console.log('test1');
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-
-    console.log('test2');
-    var userObject = {
-        "username": req.body.username,
-        "password": req.body.password,
-        "salt": Math.random().toString(36).substring(5)
-    };
-
-    //save user id in server
-    var db = req.db;
-    var users = db.get('users');
-    var userId = users.findOne().then(function(result) {
-        console.log(result); //normalReturn
-        console.log(result._id); //normalReturn
-    });
-    //res.send(userId);
-    console.log(userId);
-    var id = userId._id;
-    console.log(id);
-   // res.send(id);
-    // res.send(userId);
-
 }); //end post login
 
     /* login */
