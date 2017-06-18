7f5f53f8dfffd527f70fb44ac6dc9bf48f66fd7e
Bella <bella.gandin@gmail.com>
2017-06-18 23:53:28 +0300

    check if the user is already have a uses in server

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ var router = express.Router();
 
 var user_id = "";
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',
-      {
-          title: 'Project Security'
-      }
-  );
+router.get('/', function (req, res, next) {
+    res.render('index',
+        {
+            title: 'Project Security'
+        }
+    );
 });
 
 /* login */
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
@@ -29,11 +29,12 @@ router.post('/login', function(req, res, next) {
     var db = req.db;
     var users = db.get('users');
     //have the user in the server
-    users.findOne({ "username": req.body.username,
-        "password": req.body.password}).then(function(result) {
+    users.findOne({
+        "username": req.body.username,
+        "password": req.body.password
+    }).then(function (result) {
         console.log(result); //normalReturn
-        if (result === {}||result===null||result === undefined)
-        {
+        if (result === {} || result === null || result === undefined) {
             users.insert(userObject, function (err, doc) {
                 if (err) {
                     res.send("There was a problem adding the information to the database.");
@@ -43,14 +44,20 @@ router.post('/login', function(req, res, next) {
         }
         res.send('Successfully register to our DB.');
         console.log('Successfully register to our DB.');
-        user_id = result._id;
-        console.log(user_id);
+        users.findOne({
+            "username": req.body.username,
+            "password": req.body.password
+        }).then(function (result) {
+            console.log(result); //normalReturn
+            user_id = result._id;
+            console.log(user_id);
+        });
     });
 }); //end post login
 
-    /* login */
-    router.get('/login', function (req, res, next) {
-        res.send('LOGIN GET EXAMPLE');
-    });
+/* login */
+router.get('/login', function (req, res, next) {
+    res.send('LOGIN GET EXAMPLE');
+});
 
-    module.exports = router;
+module.exports = router;
